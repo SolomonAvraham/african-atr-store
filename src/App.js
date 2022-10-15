72d9692d3be5cfcf4276f5e4b17a7a57f321ebd6
SolomonAvraham <Selmon_a@hotmail.com>
2022-10-15 23:01:26 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
+import { Routes, Route } from "react-router";
+import React from "react";
+import {
+  Home,
+  About,
+  Contact,
+  Login,
+  PageNotFound,
+  SignUp,
+  UserProfile,
+} from "./components/pages/index";
 
 function App() {
   return (
     <div className="App">
-
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/user-profile" element={<UserProfile />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./home/Home";
+import About from "./about/About";
+import Contact from "./contact/Contact";
+import Login from "./log-in/Login";
+import PageNotFound from "./page-not-found/PageNotFound";
+import SignUp from "./sign-up/SignUp";
+import UserProfile from "./user-profile/UserProfile";
+
+export { Home, About, Contact, Login, PageNotFound, SignUp, UserProfile };
