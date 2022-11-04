2514cbef7cdd0ee8eef7c51f93c587695d4f2b28
SolomonAvraham <Selmon_a@hotmail.com>
2022-11-04 09:34:51 +0200

    update

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter basename="/home">
+      <BrowserRouter basename="/african-atr-store">
         <App />
       </BrowserRouter>
     </AuthProvider>
