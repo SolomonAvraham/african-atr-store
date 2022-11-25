596236becb3085b6b5f499b6a15d9467055734f9
SolomonAvraham <Selmon_a@hotmail.com>
2022-11-25 20:37:09 +0200

    testing

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter  >
+      <BrowserRouter basename="/african-art-store">
         <App />
       </BrowserRouter>
     </AuthProvider>
