7ffdbb0d8d56ead17ba84251435cb2c7b1eb5290
SolomonAvraham <Selmon_a@hotmail.com>
2022-12-31 19:57:04 +0200

    update

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -8,9 +8,9 @@ import {
   onAuthStateChanged,
   sendPasswordResetEmail,
 } from "firebase/auth";
-const apiKeyFirebase= process.env.REACT_APP_FIREBASE_API_KEY ||`${process.env.REACT_APP_FIREBASE_API_KEY}`
+
 const app = firebase.initializeApp({
-  apiKey: apiKeyFirebase,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORGE_BUCKET,
